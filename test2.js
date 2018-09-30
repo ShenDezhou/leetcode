07211c410d253f8820bc6516233a9560c1aac6d2
ShenDezhou <bangtech@sina.com>
2018-09-30 17:46:07 +0800

    save.

---
+++ test2.js
@@ -0,0 +1,623 @@
+let results = [{
+		"key": "1988",
+		"doc_count": 110
+}
+,
+	{
+		"key": "1987",
+		"doc_count": 101
+}
+,
+	{
+		"key": "1989",
+		"doc_count": 97
+}
+,
+	{
+		"key": "1982",
+		"doc_count": 90
+}
+,
+	{
+		"key": "1986",
+		"doc_count": 87
+}
+,
+	{
+		"key": "1990",
+		"doc_count": 86
+}
+,
+	{
+		"key": "1983",
+		"doc_count": 82
+}
+,
+	{
+		"key": "1984",
+		"doc_count": 79
+}
+,
+	{
+		"key": "1985",
+		"doc_count": 76
+}
+,
+	{
+		"key": "1978",
+		"doc_count": 69
+}
+,
+	{
+		"key": "1991",
+		"doc_count": 65
+}
+,
+	{
+		"key": "1992",
+		"doc_count": 65
+}
+,
+	{
+		"key": "1993",
+		"doc_count": 61
+}
+,
+	{
+		"key": "1981",
+		"doc_count": 60
+}
+,
+	{
+		"key": "1980",
+		"doc_count": 56
+}
+,
+	{
+		"key": "1979",
+		"doc_count": 53
+}
+,
+	{
+		"key": "1976",
+		"doc_count": 43
+}
+,
+	{
+		"key": "1968",
+		"doc_count": 42
+}
+,
+	{
+		"key": "1969",
+		"doc_count": 42
+}
+,
+	{
+		"key": "1994",
+		"doc_count": 41
+}
+,
+	{
+		"key": "1975",
+		"doc_count": 35
+}
+,
+	{
+		"key": "1973",
+		"doc_count": 34
+}
+,
+	{
+		"key": "1974",
+		"doc_count": 34
+}
+,
+	{
+		"key": "1977",
+		"doc_count": 34
+}
+,
+	{
+		"key": "1971",
+		"doc_count": 31
+}
+,
+	{
+		"key": "1972",
+		"doc_count": 31
+}
+,
+	{
+		"key": "1970",
+		"doc_count": 30
+}
+,
+	{
+		"key": "1995",
+		"doc_count": 30
+}
+,
+	{
+		"key": "1963",
+		"doc_count": 29
+}
+,
+	{
+		"key": "1967",
+		"doc_count": 27
+}
+,
+	{
+		"key": "1958",
+		"doc_count": 26
+}
+,
+	{
+		"key": "1996",
+		"doc_count": 26
+}
+,
+	{
+		"key": "1997",
+		"doc_count": 26
+}
+,
+	{
+		"key": "1954",
+		"doc_count": 25
+}
+,
+	{
+		"key": "1957",
+		"doc_count": 25
+}
+,
+	{
+		"key": "1962",
+		"doc_count": 21
+}
+,
+	{
+		"key": "1956",
+		"doc_count": 20
+}
+,
+	{
+		"key": "1964",
+		"doc_count": 17
+}
+,
+	{
+		"key": "1965",
+		"doc_count": 17
+}
+,
+	{
+		"key": "1960",
+		"doc_count": 16
+}
+,
+	{
+		"key": "1966",
+		"doc_count": 15
+}
+,
+	{
+		"key": "1999",
+		"doc_count": 15
+}
+,
+	{
+		"key": "1955",
+		"doc_count": 14
+}
+,
+	{
+		"key": "1952",
+		"doc_count": 13
+}
+,
+	{
+		"key": "1998",
+		"doc_count": 13
+}
+,
+	{
+		"key": "2000",
+		"doc_count": 13
+}
+,
+	{
+		"key": "2007",
+		"doc_count": 13
+}
+,
+	{
+		"key": "1959",
+		"doc_count": 11
+}
+,
+	{
+		"key": "1953",
+		"doc_count": 10
+}
+,
+	{
+		"key": "1961",
+		"doc_count": 10
+}
+,
+	{
+		"key": "1925",
+		"doc_count": 9
+}
+,
+	{
+		"key": "1946",
+		"doc_count": 9
+}
+,
+	{
+		"key": "1947",
+		"doc_count": 9
+}
+,
+	{
+		"key": "1920",
+		"doc_count": 8
+}
+,
+	{
+		"key": "1926",
+		"doc_count": 8
+}
+,
+	{
+		"key": "1936",
+		"doc_count": 8
+}
+,
+	{
+		"key": "1910",
+		"doc_count": 7
+}
+,
+	{
+		"key": "1919",
+		"doc_count": 7
+}
+,
+	{
+		"key": "1930",
+		"doc_count": 7
+}
+,
+	{
+		"key": "1934",
+		"doc_count": 7
+}
+,
+	{
+		"key": "2002",
+		"doc_count": 7
+}
+,
+	{
+		"key": "1915",
+		"doc_count": 6
+}
+,
+	{
+		"key": "1916",
+		"doc_count": 6
+}
+,
+	{
+		"key": "1927",
+		"doc_count": 6
+}
+,
+	{
+		"key": "1937",
+		"doc_count": 6
+}
+,
+	{
+		"key": "1940",
+		"doc_count": 6
+}
+,
+	{
+		"key": "1950",
+		"doc_count": 6
+}
+,
+	{
+		"key": "2001",
+		"doc_count": 6
+}
+,
+	{
+		"key": "2004",
+		"doc_count": 6
+}
+,
+	{
+		"key": "2006",
+		"doc_count": 6
+}
+,
+	{
+		"key": "1921",
+		"doc_count": 5
+}
+,
+	{
+		"key": "1922",
+		"doc_count": 5
+}
+,
+	{
+		"key": "1923",
+		"doc_count": 5
+}
+,
+	{
+		"key": "1924",
+		"doc_count": 5
+}
+,
+	{
+		"key": "1941",
+		"doc_count": 5
+}
+,
+	{
+		"key": "1942",
+		"doc_count": 5
+}
+,
+	{
+		"key": "1949",
+		"doc_count": 5
+}
+,
+	{
+		"key": "1951",
+		"doc_count": 5
+}
+,
+	{
+		"key": "2003",
+		"doc_count": 5
+}
+,
+	{
+		"key": "2008",
+		"doc_count": 5
+}
+,
+	{
+		"key": "1904",
+		"doc_count": 4
+}
+,
+	{
+		"key": "1914",
+		"doc_count": 4
+}
+,
+	{
+		"key": "1917",
+		"doc_count": 4
+}
+,
+	{
+		"key": "1918",
+		"doc_count": 4
+}
+,
+	{
+		"key": "1928",
+		"doc_count": 4
+}
+,
+	{
+		"key": "1929",
+		"doc_count": 4
+}
+,
+	{
+		"key": "1931",
+		"doc_count": 4
+}
+,
+	{
+		"key": "1935",
+		"doc_count": 4
+}
+,
+	{
+		"key": "1938",
+		"doc_count": 4
+}
+,
+	{
+		"key": "1948",
+		"doc_count": 4
+}
+,
+	{
+		"key": "2005",
+		"doc_count": 4
+}
+,
+	{
+		"key": "1911",
+		"doc_count": 3
+}
+,
+	{
+		"key": "1912",
+		"doc_count": 3
+}
+,
+	{
+		"key": "1943",
+		"doc_count": 3
+}
+,
+	{
+		"key": "1945",
+		"doc_count": 3
+}
+,
+	{
+		"key": "1899",
+		"doc_count": 2
+}
+,
+	{
+		"key": "1900",
+		"doc_count": 2
+}
+,
+	{
+		"key": "1902",
+		"doc_count": 2
+}
+,
+	{
+		"key": "1905",
+		"doc_count": 2
+}
+,
+	{
+		"key": "1906",
+		"doc_count": 2
+}
+,
+	{
+		"key": "1907",
+		"doc_count": 2
+}
+,
+	{
+		"key": "1913",
+		"doc_count": 2
+}
+,
+	{
+		"key": "1932",
+		"doc_count": 2
+}
+,
+	{
+		"key": "1933",
+		"doc_count": 2
+}
+,
+	{
+		"key": "1939",
+		"doc_count": 2
+}
+,
+	{
+		"key": "1715",
+		"doc_count": 1
+}
+,
+	{
+		"key": "1763",
+		"doc_count": 1
+}
+,
+	{
+		"key": "1881",
+		"doc_count": 1
+}
+,
+	{
+		"key": "1889",
+		"doc_count": 1
+}
+,
+	{
+		"key": "1894",
+		"doc_count": 1
+}
+,
+	{
+		"key": "1896",
+		"doc_count": 1
+}
+,
+	{
+		"key": "1897",
+		"doc_count": 1
+}
+,
+	{
+		"key": "1898",
+		"doc_count": 1
+}
+,
+	{
+		"key": "1901",
+		"doc_count": 1
+}
+,
+	{
+		"key": "1903",
+		"doc_count": 1
+}
+,
+	{
+		"key": "1908",
+		"doc_count": 1
+}
+,
+	{
+		"key": "1909",
+		"doc_count": 1
+}
+,
+	{
+		"key": "1944",
+		"doc_count": 1
+}
+,
+	{
+		"key": "2009",
+		"doc_count": 1
+}
+,
+	{
+		"key": "2010",
+		"doc_count": 1
+}
+,
+	{
+		"key": "2012",
+		"doc_count": 1
+}
+,
+	{
+		"key": "2013",
+		"doc_count": 1
+}];
+
+let sorted_res = results.sort((a, b) => a.key - b.key);
+console.log(sorted_res);
+//sorted_res.forEach(x => console.log(x.key + "," + x.doc_count));
+let res = "";
+for (let item of sorted_res) {
+	res += parseInt(item.key) + ",";
+}
+let val = "";
+for (let item of sorted_res) {
+	val += parseInt(item.doc_count) + ",";
+}
+console.log(res);
+console.log(val);
